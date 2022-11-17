32731153d00f2380e90ed2a430abda042305405d
PedroCorcaque <pedrollcorc@gmail.com>
2022-11-17 13:57:48 -0300

    remove codemagic from project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 
-API_URL = process.env.REACT_APP_API_URL;
-
 const make_request = () => {
   try {
-    fetch(API_URL+"/run_read_serial")
+    fetch("http://localhost:5818/run_read_serial")
     .then((response) => console.log(response));
   } catch (err) {
     console.log("Erro:", err);
